5c29edc3a299f780f005d134c62df5928947ddc1
Kenneth Brook <kenneth.brook@hotmail.com>
2026-08-11 08:59:22 -0400

    Add Mapbox configuration placeholder

---
+++ js/mapbox-config.js
@@ -0,0 +1,5 @@
+window.HC911_CONFIG = Object.freeze({
+    // Mapbox public browser token. Public tokens are intended to be visible
+    // client-side; restrict this token to hamiltontn911.gov in Mapbox.
+    mapboxAccessToken: ""
+});
